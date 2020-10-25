3f59ecc0c0bc656fc9195697c80d5129a2e54b86
vanishta <vanishta.changea@gmail.com>
2020-10-25 12:55:00 +0100

    Initial Commit

---
+++ Javascript-arrays/app.js
@@ -0,0 +1,86 @@
+// const numbers =[1,2,3];
+// console.log(numbers);
+
+// // const moreNumbers = new Array(5);
+// // console.log(moreNumbers);
+// // const moreNum = Array.from('hi');
+// // console.log(moreNum);
+
+// // const listItems = document.querySelectorAll('li');
+// // console.log(listItems);
+// // const arrayListItems = Array.from(listItems);
+// // console.log(arrayListItems);
+
+// const hobbies =['sports','cooking'];
+// hobbies.push('Reading');
+// hobbies.unshift('coding');
+// console.log(hobbies);
+// var last = hobbies.pop();
+// console.log(hobbies);
+// console.log(last);
+// hobbies.shift();
+// console.log(hobbies);
+// hobbies[1]='coding';
+// console.log(hobbies);
+// hobbies.splice(0,0, 'Good Food');
+// console.log(hobbies);
+// hobbies.splice(1,0, 'Good Drinks');
+// console.log(hobbies);
+// hobbies.splice(1,2, 'Bad Drinks');
+// console.log(hobbies);
+// hobbies.splice(1,2, ['3','4']);
+// console.log(hobbies);
+// hobbies.push(1,2, 'Great Dance');
+// console.log(hobbies);
+// hobbies. splice(-2,1);
+// console.log(hobbies);
+// hobbies. splice(-2,2);
+// console.log(hobbies);
+// const testResults = [1,2,3,4,5,6,7];
+// //console.log(testResults);
+// console.log("testResults0",testResults);
+// const storedResults = testResults.slice(0,7);
+// const storedResults2 = testResults.slice(0,2);
+// const storedResults3 = testResults.slice(-3,2);
+// console.log("testResults",testResults);
+// console.log("storedResults", storedResults);
+// console.log("storedResults2",storedResults2);
+// console.log("storedResults3",storedResults3);
+// testResults.push(5.92);
+
+// console.log("testResults",testResults,"storedResults",storedResults);
+// const storedResults4 = testResults.slice(-3,-2);
+// console.log("storedResults4 - -3,-2",storedResults4);
+
+// const arr1 = [1,2,3,4,"value 1"];
+// const arr2 = arr1.push([5,6,7]);
+// console.log("arr1",arr1);
+// console.log("arr2",arr2);
+// arr1.push([11,22,33]);
+// console.log("arr1",arr1);
+// const arr3 = arr1.concat([8,9,10]);
+// console.log("arr3", arr3);
+
+// const arr4 = [12,20,13,14,15,"hello"];
+// const arr5 = arr4.concat([10,20,30]);
+// console.log(arr5);
+// console.log(arr5.indexOf(20));
+// //IndexOf does not work with objects
+// const arr6 = [{name:"Vanishta"}, {name:"Chris"}, {name:"Chris2"}];
+// console.log(arr6);
+// console.log(arr6.indexOf({name:"Chris"}));
+
+const personData = [{name:'Max'}, {name:'Karl'}, {name : 'Alex'}];
+console.log(personData.indexOf({name:'Karl'}));
+const karl = personData.find((person, idx, persons)=>{
+   //console.log(person.name);
+   return person.name ==='Karl';
+});
+karl.name='Andras'
+console.log(karl, personData);
+
+const maxIndex = personData.findIndex((person, idx, persons)=>{
+   //console.log(person.name);
+   return person.name ==='Max';
+});
+console.log("maxIndex", maxIndex);
