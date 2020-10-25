b1b0d34f1f6479d6f8c03a1dabe25c94bb5ff94e
vanishta <vanishta.changea@gmail.com>
2020-10-25 13:28:41 +0100

    foreach map

--- Javascript-arrays/app.js
+++ Javascript-arrays/app.js
@@ -70,17 +70,52 @@
 // console.log(arr6);
 // console.log(arr6.indexOf({name:"Chris"}));
 
-const personData = [{name:'Max'}, {name:'Karl'}, {name : 'Alex'}];
-console.log(personData.indexOf({name:'Karl'}));
-const karl = personData.find((person, idx, persons)=>{
-   //console.log(person.name);
-   return person.name ==='Karl';
-});
-karl.name='Andras'
-console.log(karl, personData);
+// const personData = [{name:'Max'}, {name:'Karl'}, {name : 'Alex'}];
+// console.log(personData.indexOf({name:'Karl'}));
+// const karl = personData.find((person, idx, persons)=>{
+//    //console.log(person.name);
+//    return person.name ==='Karl';
+// });
+// karl.name='Andras'
+// console.log(karl, personData);
 
-const maxIndex = personData.findIndex((person, idx, persons)=>{
-   //console.log(person.name);
-   return person.name ==='Max';
+// const maxIndex = personData.findIndex((person, idx, persons)=>{
+//    //console.log(person.name);
+//    return person.name ==='Max';
+// });
+// console.log("maxIndex", maxIndex);
+
+// const arr7 = ["adfs","test","hi"];
+// console.log(arr7.includes("test"));
+// console.log(arr7.includes("test44"));
+
+const prices = [110.99,5.25,3.96,6.59,8.45]
+const tax = 0.05;
+const taxAdjustedPrices = [];
+for(const price of prices){
+    taxAdjustedPrices.push(price * (1 + tax));
+}
+console.log(taxAdjustedPrices);
+const taxAdjustedPrices2=[];
+prices.forEach((price, index, prices)=>{
+    const priceObj = {
+        index, 
+        price,
+        taxAdjustedPrice: price * (1 + tax)
+        }
+    
+    taxAdjustedPrices2.push(priceObj);
 });
-console.log("maxIndex", maxIndex);
+console.log(taxAdjustedPrices2);
+//const taxAdjustedPrices3=[];
+const taxAdjustedPrices3 = prices.map((price, index, prices)=>{
+    const priceObj = {
+        index, 
+        price,
+        taxAdjustedPrice: price * (1 + tax)
+        };
+    
+    return priceObj;
+   // return taxAdjustedPrices3;
+});
+console.log(taxAdjustedPrices3);
